2221c5532155f03a59fd739697550bb24189f4cc
Jérémy Leherpeur <jeremy.leherpeur@yousign.com>
2020-07-31 12:41:34 +0200

    Add CI / phpunit / php-cs-fixer / phpstan

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class             => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class            => ['dev' => true],
 ];
