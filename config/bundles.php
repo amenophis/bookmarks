b02c50bd35e3dd1c3eb01b734c17f8622cfd1867
Jérémy Leherpeur <jeremy.leherpeur@yousign.com>
2020-07-31 12:39:45 +0200

    Prepare stack + upgrade to symfony 4.4.*

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
